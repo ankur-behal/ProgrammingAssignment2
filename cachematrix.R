03919cac8dfc322feaf1b7ae2916995127d304ba
Ankur-Behal <ankur_behal@hotmail.com>
2017-07-16 12:30:21 +0530

    Modified as part of assignment

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,17 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+	invmtrx <- NULL
+	set <- function(y) {
+		x <<- y ## << is used to assign a variable in an environment which is not in current environment
+		invmtrx <<- NULL
+		
+	}
+	get <- function () x
+	setinverse <- function(inverse) invmtrx <- inverse
+	getinverse <- function ()  invmtrx
+	list (set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+
 }
 
 
@@ -12,4 +23,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+        invmtrx <- x$getinverse()
+	if(!is.null(invmtrx)) {
+		message ("Getting Inverse From Cache")
+		return (invmtrx)
+	}
+	inv <- x$get()
+	invmtrx <- solve(inv)
+	x$setinverse(invmtrx)
+	invmtrx
+
 }
